c8ff86f065142e5421ec8384fb67cc3bc6d12877
Danyal Imran <dan.imran97@gmail.com>
2023-01-23 10:41:57 -0500

    added a close function so when the task is complete and the user clicks "close" it will change from open to close

--- main.js
+++ main.js
@@ -67,3 +67,17 @@ function saveIssue(e) {
 
    e.preventDefault()
 }
+
+
+function setStatusClosed(id) {
+   let issues = JSON.parse(localStorage.getItem('issues'))
+   for(let i = 0; i < issues.length; i++) {
+       if(issues[i].id === id) {
+         issues[i].status = "Closed"
+       }
+   }
+
+   localStorage.setItem('issues', JSON.stringify(issues))
+
+   fetchIssues()
+}
