fd0ab366f4e6b4ec5a77fbabe6df70f1a102c338
Danyal Imran <dan.imran97@gmail.com>
2023-01-24 12:09:40 -0500

    added a function that saves to localStorage

--- main.js
+++ main.js
@@ -86,7 +86,12 @@ function deleteIssue (id) {
    let issues = JSON.parse(localStorage.getItem('issues'))
    for(let i = 0; i < issues.length; i++) {
       if(issues[i].id === id) {
-         
+         issues.splice(i,1)
       }
    }
+
+   localStorage.setItem('issues', JSON.stringify(issues))
+
+   fetchIssues()
+   
 }
